6e9c705b5f698906b3a5e75e7bfbacef31ac579f
DmitriWeb <dmitrimnst@gmail.com>
2023-12-28 21:56:22 +0400

    lesson-2.2

---
+++ app.js
@@ -0,0 +1,5 @@
+
+let firstName = 'Dmitri';
+let myAge = '35';
+let mySity = 'Batumi';
+let myHobby = 'design';
---
+++ lesson-2.2/lesson-2.2.js
@@ -0,0 +1,9 @@
+let firstName = 'Dmitri';
+let myAge = '35';
+let mySity = 'Batumi';
+let myHobby = 'design';
+
+console.log(firstName);
+console.log(myAge);
+console.log(mySity);
+console.log(myHobby);
