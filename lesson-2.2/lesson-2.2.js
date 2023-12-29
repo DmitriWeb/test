b0d46b72e89ce32eebc9e5640de758a5f255017c
DmitriWeb <dmitrimnst@gmail.com>
2023-12-29 16:02:23 +0400

    lesson-2.2

--- lesson-2.2/lesson-2.2.js
+++ lesson-2.2/lesson-2.2.js
@@ -3,7 +3,7 @@ let myAge = '35';
 let mySity = 'Batumi';
 let myHobby = 'design';
 
-console.log('Dmitri' firstName);
-console.log('35' myAge);
-console.log('Batumi' mySity);
-console.log('design' myHobby);
+console.log('Dmitri', firstName);
+console.log('35', myAge);
+console.log('Batumi', mySity);
+console.log('design', myHobby);
