72515ee038615843d03e415eaa2d3c9b90a5cb6a
DmitriWeb <dmitrimnst@gmail.com>
2024-01-03 20:51:08 +0400

    lesson-2.2

---
+++ lesson-2.3/lesson-2.3.js
@@ -0,0 +1,6 @@
+let titleProject = 'Оценка стоимости'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 15000
+let percentage = 15
+let fullPrice = 150000
+let responsive = true
